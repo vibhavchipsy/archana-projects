820a44c500ecef1c427eaab9362926ac6183bf42
vibhav <kamathpvk@gmail.com>
2024-10-16 09:04:43 +0530

    slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { OurValuesComponent } from '../our-values/our-values.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { HomepageBannerComponent } from '../homepage-banner/homepage-banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent, OurValuesComponent, ProjectsComponent],
+  imports: [
+    AboutComponent,
+    OurValuesComponent,
+    ProjectsComponent,
+    HomepageBannerComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ src/app/homepage-banner/homepage-banner.component.ts
@@ -0,0 +1,35 @@
+import { Component, AfterViewInit } from '@angular/core';
+import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-homepage-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage-banner.component.html',
+  styleUrl: './homepage-banner.component.css',
+})
+export class HomepageBannerComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    if (typeof document !== 'undefined') {
+      const swiper = new Swiper('.swiper', {
+        modules: [Navigation, Pagination],
+        loop: true,
+        autoplay: {
+          delay: 1000,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: (index: number, className: string): string => {
+            return `<span class="${className}">${index + 1}</span>`;
+          },
+        },
+      });
+    }
+  }
+}
